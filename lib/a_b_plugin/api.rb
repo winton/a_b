924386219bb43d758bd41bf02f43de5f17c233b1
winton <mail@wintoni.us>
2010-06-15 11:18:36 -0700

    API.create_category, create_env, and create_site

--- lib/a_b_plugin/api.rb
+++ lib/a_b_plugin/api.rb
@@ -3,34 +3,67 @@ class ABPlugin
     
     include HTTParty
     
+    def self.create_category(attributes)
+      return unless Config.token && Config.url
+      base_uri Config.url
+      post('/categories/create.json', :query => compress(
+        :token => attributes.delete(:token) || Config.token,
+        :category => attributes
+      ))
+    end
+    
+    def self.create_env(attributes)
+      return unless Config.token && Config.url
+      base_uri Config.url
+      post('/envs/create.json', :query => compress(
+        :token => attributes.delete(:token) || Config.token,
+        :env => attributes
+      ))
+    end
+    
+    def self.create_site(attributes)
+      return unless Config.token && Config.url
+      base_uri Config.url
+      post('/sites/create.json', :query => compress(
+        :token => attributes.delete(:token) || Config.token,
+        :site => attributes
+      ))
+    end
+    
     def self.create_user(attributes)
       return unless Config.token && Config.url
       base_uri Config.url
-      post('/users/create.json', :query => {
-        :token => Config.token,
+      post('/users/create.json', :query => compress(
+        :token => attributes.delete(:token) || Config.token,
         :user => attributes
-      })
+      ))
     end
     
     def self.site(options={})
       return unless Config.token && Config.url && (options[:name] || Config.site)
       base_uri Config.url
-      get('/site.json', :query => {
+      get('/site.json', :query => compress(
         :include => options[:include],
         :name => options[:name] || Config.site,
         :only => options[:only],
         :token => Config.token
-      })
+      ))
     end
     
     def self.sites(options={})
       return unless Config.token && Config.url
       base_uri Config.url
-      get('/sites.json', :query => {
+      get('/sites.json', :query => compress(
         :include => options[:include],
         :only => options[:only],
-        :token => Config.token
-      })
+        :token => options[:token] || Config.token
+      ))
+    end
+    
+    private
+    
+    def self.compress(hash)
+      hash.delete_if { |key, value| value.nil? }
     end
   end
 end
